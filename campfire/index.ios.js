4f53f5c841e0b883330c16d87c18e98ec9551f12
Titus Woo <titus.woo@me.com>
2015-09-25 22:50:42 -0400

    added campfire

--- campfire/index.ios.js
+++ campfire/index.ios.js
@@ -17,7 +17,7 @@ var campfire = React.createClass({
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Campfire!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
